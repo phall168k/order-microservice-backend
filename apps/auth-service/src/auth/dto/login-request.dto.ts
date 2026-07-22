2f73e6933e426487a48c7b3b12569540c4c568ab
phall168k <phall168k@gmail.com>
2026-07-22 22:47:54 +0700

    ft: add default user credential for login

--- apps/auth-service/src/auth/dto/login-request.dto.ts
+++ apps/auth-service/src/auth/dto/login-request.dto.ts
@@ -2,11 +2,17 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
 export class LoginRequestDto {
-    @ApiProperty()
+    @ApiProperty({
+        type: String,
+        example: "Admin",
+    })
     @IsNotEmpty()
     username: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        type: String,
+        example: "123"
+    })
     @IsNotEmpty()
     password: string;
 }
